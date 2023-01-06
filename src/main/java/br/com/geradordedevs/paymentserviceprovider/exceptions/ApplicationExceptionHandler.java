6067c2b5aa6ee53492bf856440acc8b74790e482
Mateus Ferian <mateusferian10@gmail.com>
2023-01-06 09:57:12 -0300

    :sparkles: feat: creating ApplicationExceptionHandler from layer

---
+++ src/main/java/br/com/geradordedevs/paymentserviceprovider/exceptions/ApplicationExceptionHandler.java
@@ -0,0 +1,44 @@
+package br.com.geradordedevs.paymentserviceprovider.exceptions;
+
+import br.com.geradordedevs.paymentserviceprovider.exceptions.models.ErrorObject;
+import br.com.geradordedevs.paymentserviceprovider.exceptions.models.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<ErrorObject> errors = getErrors(ex);
+        ErrorResponse errorResponse = getErrorResponse( status, errors);
+        log.error("erro de entrada de dados: {}", errorResponse);
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+    private ErrorResponse getErrorResponse( HttpStatus status, List<ErrorObject> errors) {
+        return new ErrorResponse(
+                Instant.now().toEpochMilli(),
+                status.value(),
+                status.getReasonPhrase(),
+                "Requisição possui campos inválidos", errors);
+    }
+
+    private List<ErrorObject> getErrors(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> new ErrorObject(error.getDefaultMessage(), error.getField(), error.getRejectedValue()))
+                .collect(Collectors.toList());
+    }
+    }
+
