f0371462c69ff02b265f2c8f35f3c4ae1f34a8e8
Mateus Ferian <mateusferian10@gmail.com>
2023-01-08 13:36:18 -0300

    :sparkles: feat: creating logic for creating accounts payable in TransactionsMapper

--- src/main/java/br/com/geradordedevs/paymentserviceprovider/mappers/TransactionsMapper.java
+++ src/main/java/br/com/geradordedevs/paymentserviceprovider/mappers/TransactionsMapper.java
@@ -2,13 +2,21 @@ package br.com.geradordedevs.paymentserviceprovider.mappers;
 
 import br.com.geradordedevs.paymentserviceprovider.dtos.requests.TransactionsRequestDTO;
 import br.com.geradordedevs.paymentserviceprovider.dtos.responses.TransactionsResponseDTO;
+import br.com.geradordedevs.paymentserviceprovider.entities.PayableEntity;
 import br.com.geradordedevs.paymentserviceprovider.entities.TransactionsEntity;
+import br.com.geradordedevs.paymentserviceprovider.enums.PaymentMethodEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,19 +29,33 @@ public class TransactionsMapper {
     @Autowired
     private final ModelMapper mapper;
 
+
     public TransactionsResponseDTO toDto(TransactionsEntity entity){
         log.info("converting entity{} to dto", entity);
+
+            PayableEntity payableEntity = new PayableEntity();
+        if (entity.getPaymentMethod() == PaymentMethodEnum.CREDIT_CARD) {
+            payableEntity.setId(entity.getId());
+            payableEntity.setPaymentDate(LocalDate.now().plusDays(30));
+            payableEntity.setStatus(PaymentMethodEnum.CREDIT_CARD.getStatus());
+            entity.setPayables(payableEntity);
+        }
+        if (entity.getPaymentMethod() == PaymentMethodEnum.DEBIT_CARD) {
+            payableEntity.setId(entity.getId());
+            payableEntity.setPaymentDate(LocalDate.now());
+            payableEntity.setStatus(PaymentMethodEnum.DEBIT_CARD.getStatus());
+            entity.setPayables(payableEntity);
+        }
+
+        log.info("adding payable field {}", entity);
         return  mapper.map(entity, TransactionsResponseDTO.class);
     }
 
 
     public TransactionsEntity toEntity(TransactionsRequestDTO request){
         log.info("converting dto{} to entity", request);
-        return  mapper.map(request, TransactionsEntity.class);
+        return  mapper.map(request,TransactionsEntity.class);
     }
-
-
-
 
     public List<TransactionsResponseDTO> toDtoList(Iterable<TransactionsEntity> lista){
         log.info("converting entity list{} to dto list", lista);
