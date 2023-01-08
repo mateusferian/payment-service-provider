9684aef005ca16bc478c865d413363b4afafc632
Mateus Ferian <mateusferian10@gmail.com>
2023-01-08 13:39:33 -0300

    :recycle: refactor: removing attributes that were not being used

--- src/main/java/br/com/geradordedevs/paymentserviceprovider/entities/PayableEntity.java
+++ src/main/java/br/com/geradordedevs/paymentserviceprovider/entities/PayableEntity.java
@@ -1,6 +1,5 @@
 package br.com.geradordedevs.paymentserviceprovider.entities;
 
-import br.com.geradordedevs.paymentserviceprovider.enums.PaymentMethodEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.Instant;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -21,8 +19,7 @@ public class PayableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private  Long id;
-    private PaymentMethodEnum status;
-    private Instant TransactionCreationDate;
-    private String paymentDate;
-    private  double fee;
+    private String status;
+    private LocalDate paymentDate;
+
 }
