7c55ed8e029b2a540fe6004304b08602b87573ae
Mateus Ferian <mateusferian10@gmail.com>
2023-01-09 10:51:56 -0300

    :sparkles: feat: adding bean validation to project

--- src/main/java/br/com/geradordedevs/paymentserviceprovider/dtos/requests/TransactionsRequestDTO.java
+++ src/main/java/br/com/geradordedevs/paymentserviceprovider/dtos/requests/TransactionsRequestDTO.java
@@ -5,8 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -15,19 +14,31 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class TransactionsRequestDTO {
 
-
+    @NotNull(message = "{null.field}")
+    @Min(value = 10, message = "{transaction.below.the.minimum}")
     private BigDecimal transactionAmount;
 
+    @NotBlank(message = "{white.field}")
+    @Size(min = 30 ,max = 100, message = "{size.invalid}")
     private  String transactionDescription;
 
+    @NotNull(message = "{null.field}")
     private PaymentMethodEnum paymentMethod;
 
+    @NotBlank(message = "{white.field}")
+    @Size(min = 2 ,max = 40, message = "{size.invalid}")
     private String cardNumber;
 
+    @NotBlank(message = "{white.field}")
+    @Size(min = 2 ,max = 30, message = "{size.invalid}")
     private String bearerName;
 
+    @NotBlank(message = "{white.field}")
+    @Size(min = 5 ,max = 5, message = "{size.invalid}")
     private String cardExpirationDate;
 
+    @NotNull(message = "{null.field}")
+    @Max(value = 999, message = "{transaction.above.the.maximum}")
     private  int cvv;
 
 }
