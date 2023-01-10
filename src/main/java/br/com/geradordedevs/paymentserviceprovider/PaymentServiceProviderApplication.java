f92ea43e99c62f7dc7d650a1834d70d0462f9220
Mateus Ferian <mateusferian10@gmail.com>
2023-01-10 20:22:02 -0300

    :recycle: refactor: removing method that is no longer being used

--- src/main/java/br/com/geradordedevs/paymentserviceprovider/PaymentServiceProviderApplication.java
+++ src/main/java/br/com/geradordedevs/paymentserviceprovider/PaymentServiceProviderApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PaymentServiceProviderApplication {
@@ -18,9 +17,4 @@ public class PaymentServiceProviderApplication {
 	public ModelMapper modelMapper(){
 		return  new ModelMapper();
 	}
-	@Bean
-	public BCryptPasswordEncoder encoder(){
-		return new  BCryptPasswordEncoder();
-	}
-
 }
