96a3d32689127dcf008aaf43c0698ed579b9614a
Mateus Ferian <mateusferian10@gmail.com>
2023-01-10 10:02:03 -0300

    :sparkles: feat: creating custom query

--- src/main/java/br/com/geradordedevs/paymentserviceprovider/repositories/TransactionsRepository.java
+++ src/main/java/br/com/geradordedevs/paymentserviceprovider/repositories/TransactionsRepository.java
@@ -2,6 +2,12 @@ package br.com.geradordedevs.paymentserviceprovider.repositories;
 
 import br.com.geradordedevs.paymentserviceprovider.entities.TransactionsEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TransactionsRepository extends CrudRepository<TransactionsEntity,Long> {
+
+    List<TransactionsEntity> findByNameContaining (String name);
 }
