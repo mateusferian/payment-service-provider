bfa0a9ad2c05af9386e55d307e1321c379b6c559
Mateus Ferian <mateusferian10@gmail.com>
2023-01-11 16:38:10 -0300

    :test_tube: test: creating tests for the services layer

---
+++ src/test/java/br/com/geradordedevs/paymentserviceprovider/services/impl/PayableServiceImplTest.java
@@ -0,0 +1,55 @@
+package br.com.geradordedevs.paymentserviceprovider.services.impl;
+
+import br.com.geradordedevs.paymentserviceprovider.entities.PayableEntity;
+import br.com.geradordedevs.paymentserviceprovider.enums.PaymentMethodEnum;
+import br.com.geradordedevs.paymentserviceprovider.repositories.PayableRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class PayableServiceImplTest {
+
+    @InjectMocks
+    private PayableServiceImpl payableService;
+
+    @Mock
+    private PayableRepository payableRepository;
+
+    @Before
+    public void setupMock() {
+        MockitoAnnotations.openMocks(this);
+        when(payableRepository.save(returnObjectPayableEntityMethodDebit())).thenReturn(returnObjectPayableEntityMethodDebit());
+        when(payableRepository.save(returnObjectPayableEntityMethodCredit())).thenReturn(returnObjectPayableEntityMethodCredit());
+    }
+
+    @Test
+    public  void  savePayableWipingTheCardInTheFromMethodDebit() {
+        assertEquals(returnObjectPayableEntityMethodDebit(), payableService.savePayable(PaymentMethodEnum.DEBIT_CARD));
+    }
+
+    @Test
+    public  void  savePayablesWipingTheCardInTheFromMethodCredit() {
+        assertEquals(returnObjectPayableEntityMethodCredit(), payableService.savePayable(PaymentMethodEnum.CREDIT_CARD));
+    }
+
+    private PayableEntity returnObjectPayableEntityMethodDebit(){
+        return  new PayableEntity(PaymentMethodEnum.DEBIT_CARD.getStatus(), LocalDate.now());
+    }
+
+    private PayableEntity returnObjectPayableEntityMethodCredit(){
+        return  new PayableEntity(PaymentMethodEnum.CREDIT_CARD.getStatus(),LocalDate.now().plusDays(30));
+    }
+}
+
---
+++ src/test/java/br/com/geradordedevs/paymentserviceprovider/services/impl/TransactionsServiceImplTest.java
@@ -0,0 +1,97 @@
+package br.com.geradordedevs.paymentserviceprovider.services.impl;
+
+import br.com.geradordedevs.paymentserviceprovider.entities.PayableEntity;
+import br.com.geradordedevs.paymentserviceprovider.entities.TransactionsEntity;
+import br.com.geradordedevs.paymentserviceprovider.enums.PaymentMethodEnum;
+import br.com.geradordedevs.paymentserviceprovider.repositories.TransactionsRepository;
+import br.com.geradordedevs.paymentserviceprovider.services.TransactionsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class TransactionsServiceImplTest {
+
+    @InjectMocks
+    private  TransactionsServiceImpl transactionsService;
+
+    @Mock
+    private TransactionsRepository transactionsRepository;
+
+    private  final Long MOCK_ID = 1l;
+    private final BigDecimal MOCK_TRAMSACTION_AMOUNT = new BigDecimal(100);
+    private  final String MOCK_TRANSACTION_DESCRIPTION = "fazendo teste unitario com tudo feito e funcionando corretamente";
+    private  final PaymentMethodEnum MOCK_PAYMENT_METHOD_ENUM = PaymentMethodEnum.DEBIT_CARD;
+    private  final  String MOCK_CARD_NUMBER = "1234567890123456789";
+    private  final  String MOCK_NAME = "carlos";
+    private  final  String MOCK_CARD_EXPIRATION_DATE = "10/22";
+    private  final  int MOCK_CVV = 888;
+
+    private  final Long MOCK_ID_PAYABLE = 1l;
+    private  final  String STATUS= PaymentMethodEnum.DEBIT_CARD.getStatus();
+    private  final LocalDate PAYMENT_DATE= LocalDate.now();
+
+
+    @Before
+    public void setupMock(){
+        MockitoAnnotations.openMocks(this);
+        when(transactionsRepository.save(returnObjectOfficeEntity())).thenReturn(returnObjectOfficeEntity());
+        when(transactionsRepository.findByNameContaining(MOCK_NAME)).thenReturn(returnListAllTransactionEntity());
+    }
+
+    @Test
+    public void  findAllByName(){
+        assertEquals(returnListAllTransactionEntity(),transactionsService.findAllByName(MOCK_NAME));
+    }
+
+    @Test
+    public void saveTransactionMustReturnOk(){
+        assertEquals(returnObjectOfficeEntity(),transactionsService.save(returnObjectOfficeEntity()));
+    }
+
+    @Test
+    public void  findAllBalanceByName(){
+        assertEquals(returnListAllTransactionEntity(),transactionsService.findAllByName(MOCK_NAME));
+    }
+
+    private List<TransactionsEntity> returnListAllTransactionEntity() {
+        List<TransactionsEntity> transactionsEntityList = new ArrayList<>();
+        transactionsEntityList.add(returnObjectOfficeEntityId());
+
+        return transactionsEntityList;
+    }
+
+
+
+    private TransactionsEntity returnObjectOfficeEntity(){
+        return  new TransactionsEntity(MOCK_TRAMSACTION_AMOUNT,MOCK_TRANSACTION_DESCRIPTION,MOCK_PAYMENT_METHOD_ENUM,MOCK_CARD_NUMBER,
+                MOCK_NAME,MOCK_CARD_EXPIRATION_DATE,MOCK_CVV, returnObjectPayableEntity());
+    }
+
+    private PayableEntity returnObjectPayableEntity(){
+        return  new PayableEntity(PaymentMethodEnum.DEBIT_CARD.getStatus(),LocalDate.now());
+    }
+
+    private TransactionsEntity returnObjectOfficeEntityId(){
+        return  new TransactionsEntity(MOCK_ID,MOCK_TRAMSACTION_AMOUNT,MOCK_TRANSACTION_DESCRIPTION,MOCK_PAYMENT_METHOD_ENUM,MOCK_CARD_NUMBER,
+                MOCK_NAME,MOCK_CARD_EXPIRATION_DATE,MOCK_CVV,  returnObjectPayableEntityId());
+    }
+
+    private PayableEntity returnObjectPayableEntityId(){
+        return  new PayableEntity(MOCK_ID_PAYABLE,PaymentMethodEnum.DEBIT_CARD.getStatus(),LocalDate.now());
+    }
+}
